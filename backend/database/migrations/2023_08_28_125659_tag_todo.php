58a0bd52fa1f40d3c37dd9e235dc17401af54e63
john garcia <garcia.johnray25@gmail.com>
2023-08-28 22:49:32 +0800

    setup backend; added models and migrations

---
+++ backend/database/migrations/2023_08_28_125659_tag_todo.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+
+        Schema::create('tag_todo', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('tag_id');
+            $table->unsignedBigInteger('todo_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::drop('tag_todo');
+    }
+};
